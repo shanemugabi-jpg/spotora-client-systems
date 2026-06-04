9e2c171e16297f4113b41bc835f819a789a5087c
shanemugabi-jpg <shanemugabi@gmail.com>
2026-06-04 19:17:58 +0300

    Define app metadata and RootLayout component
    Add metadata and layout structure for the app

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,16 @@
+import "./globals.css";
 
+export const metadata = {
+  title: "Zone 7",
+  description: "QR Ordering by Spotora",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+      </body>
+    </html>
+  );
+}
