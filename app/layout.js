48e45183b59c382b16e627e7c267a6c78b60f90d
shanemugabi-jpg <shanemugabi@gmail.com>
2026-06-04 19:26:40 +0300

    Update title in metadata and format body element

--- app/layout.js
+++ app/layout.js
@@ -1,16 +1,20 @@
 import "./globals.css";
 
 export const metadata = {
-  title: "Zone 7",
+  title: "Zone 7 Menu",
   description: "QR Ordering by Spotora",
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
+  maximumScale: 1,
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
